2219330d7b3873124bae8d2ad813518951c75baf
Jonathan Penny <jon@jonppenny.co.uk>
2025-06-04 20:35:33 +0100

    Switch to more modern async/await for loading modules

--- _assets/scripts/app.js
+++ _assets/scripts/app.js
@@ -1,9 +1,25 @@
 'use strict';
 
 import 'bootstrap';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-import * as Helpers from "./helpers/helpers.js";
+import domReady from './domReady.js';
 
-window.addEventListener('DOMContentLoaded', function () {
-	console.log(Helpers.version());
+domReady(async () => {
+    const string = document.querySelector('.myString');
+    if (string) {
+        const module = await import('./helpers/helpers.js');
+        module.wordWrap(string);
+    }
+
+    const loaderModule = await import('./loader.js');
+    await loaderModule.default.init('.paragraph', './templates/paragraph', 'paragraph--type--');
+    await loaderModule.default.init('.block', './templates/block', 'block--');
+
+    AOS.init({
+        easing:   'ease-out-cubic',
+        duration: 600,
+        once:     true,
+    });
 });
